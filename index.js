5695f7b5b50e3cd19ef16fb68adfad5ef04c4afa
Makhfous Diallo <makhfous.diallo@gmail.com>
2020-04-06 02:45:51 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+
+ const PORT = process.env.PORT || 5000;
+
+
+const otherstuff = "this is some news stuff....";
+ //Set handlebars middlewar
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+//Set handlebars routes
+app.get('/', function (req, res) {
+    res.render('home',{
+    	stuff: otherstuff,
+    	});
+});
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+ app.listen(PORT, () => console.log('Server Listening on port ' + PORT ));
