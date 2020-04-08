6b231c8e643190d538bdf8f15e344656c9dcc0be
Makhfous Diallo <makhfous.diallo@gmail.com>
2020-04-08 18:05:27 -0400

    created procfile

--- index.js
+++ index.js
@@ -2,20 +2,57 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const request = require('request');
 
  const PORT = process.env.PORT || 5000;
 
+ //use body parser middleware
+ app.use(bodyParser.urlencoded({extended: false}));
+
+// API KEYS pk_3bb091ff17514ca0915a0b135d46e392
+//Create call API function
+function call_api(finishedAPI, ticker){
+	request('https://cloud.iexapis.com/stable/stock/'+ ticker +'/quote?token=pk_3bb091ff17514ca0915a0b135d46e392', { json: true }, (err ,res , body) => {
+	if (err) {return console.log(err);}
+	if(res.statusCode === 200){
+		//console.log(body)
+		finishedAPI(body);
+		};
+	});
+};
+
+
 
 const otherstuff = "this is some news stuff....";
+const funct = "Dieu est Grand !"
  //Set handlebars middlewar
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
  
-//Set handlebars routes
+//Set handlebars index get routes
 app.get('/', function (req, res) {
-    res.render('home',{
-    	stuff: otherstuff,
+	call_api(function(doneAPI){
+		res.render('home',{
+    	stock: doneAPI,
+    	});
+	}, 'tsla');
+});
+
+//Set handlebars index post routes
+app.post('/', function (req, res) {
+	call_api(function(doneAPI){
+		//posted_stock = req.body.stock_ticker;
+		res.render('home',{
+    	stock: doneAPI,
+    	});
+	}, req.body.stock_ticker);
+});
+
+//Create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about',{
+    	stuff2: funct,
     	});
 });
 
